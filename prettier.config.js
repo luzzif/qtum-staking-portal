1b9d243b017d57402904a02325f923edf6631d8d
Paolo Guerra <guerra.cico@hotmail.it>
2019-10-24 19:36:33 +0200

    lint: add prettier config file)

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    tabWidth: 4,
+    proseWrap: "always",
+    overrides: [
+        {
+            files: "*.md",
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
+};
