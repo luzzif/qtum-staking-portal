3edcd1820f8c586d9e2b406f3ef14e867ad67abf
Paolo Guerra <guerra.cico@hotmail.it>
2019-10-24 19:37:59 +0200

    feat: add expected block time

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { addressInfoReducer } from "./balances";
+import { blockchainInfoReducer } from "./blockchain-info"
 import { loadingsReducer } from "./loadings";
 import { pricesReducer } from "./prices";
 
 export const reducers = {
+    blockchainInfo: blockchainInfoReducer,
     addressInfo: addressInfoReducer,
     loadings: loadingsReducer,
     prices: pricesReducer
